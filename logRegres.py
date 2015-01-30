e3478a8910b4bd19840240dfe2e2020d2c0fc284
Ji ZHANG <jizhang@anjuke.com>
2015-01-30 12:40:09 +0800

    use nparray

--- logRegres.py
+++ logRegres.py
@@ -9,44 +9,38 @@ def loadDataSet():
     for line in fr.readlines():
         lineArr = line.strip().split()
         dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
-        labelMat.append(int(lineArr[2]))
-    return dataMat, labelMat
+        labelMat.append([int(lineArr[2])])
+    return np.array(dataMat), np.array(labelMat)
 
 def sigmoid(inX):
     return 1.0 / (1.0 + np.exp(-inX))
 
-def gradAscent(dataMatIn, classLabels):
-    dataMatrix = np.mat(dataMatIn)
-    labelMat = np.mat(classLabels).T
-    m, n = dataMatrix.shape
+def gradAscent(dataMat, labelMat):
+    m, n = dataMat.shape
     alpha = 0.001
     maxCycles = 500
     weights = np.ones((n, 1))
     for k in range(maxCycles):
-        h = sigmoid(dataMatrix * weights)
+        h = sigmoid(dataMat.dot(weights))
         error = (labelMat - h)
-        weights = weights + alpha * dataMatrix.T * error
+        weights = weights + alpha * dataMat.T.dot(error)
     return weights
 
-dataMatIn, classLabels = loadDataSet()
-dataMat = np.mat(dataMatIn)
-labelMat = np.mat(classLabels).T
+dataMat, labelMat = classLabels = loadDataSet()
 
-wei = gradAscent(dataMatIn, classLabels)
+wei = gradAscent(dataMat, labelMat)
 
-def plotBestFit(wei):
+def plotBestFit(weights):
     import matplotlib.pyplot as plt
-    weights = wei.getA()
     dataMat, labelMat = loadDataSet()
-    dataArr = np.array(dataMat)
-    n = dataArr.shape[0]
+    n = dataMat.shape[0]
     xcord1 = []; ycord1 = []
     xcord2 = []; ycord2 = []
     for i in range(n):
         if int(labelMat[i]) == 1:
-            xcord1.append(dataArr[i, 1]); ycord1.append(dataArr[i, 2])
+            xcord1.append(dataMat[i, 1]); ycord1.append(dataMat[i, 2])
         else:
-            xcord2.append(dataArr[i, 1]); ycord2.append(dataArr[i, 2])
+            xcord2.append(dataMat[i, 1]); ycord2.append(dataMat[i, 2])
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
@@ -57,32 +51,30 @@ def plotBestFit(wei):
     plt.xlabel('X1'); plt.ylabel('X2')
     plt.show()
 
-def stocGradAscent0(dataMatrix, classLabels):
-    m, n = dataMatrix.shape
+def stocGradAscent0(dataMat, labelMat):
+    m, n = dataMat.shape
     alpha = 0.01
     weights = np.ones(n)
     for i in range(m):
-        h = sigmoid(np.sum(dataMatrix[i] * weights))
-        error = classLabels[i] - h
-        weights = weights + alpha * error * dataMatrix[i]
+        h = sigmoid(np.sum(dataMat[i].dot(weights)))
+        error = labelMat[i] - h
+        weights = weights + alpha * error * dataMat[i]
     return weights
 
-wei0 = stocGradAscent0(np.array(dataMatIn), classLabels)
+wei0 = stocGradAscent0(dataMat, labelMat)
 
-# plotBestFit(np.mat(wei0).T)
-
-def stocGradAscent1(dataMatrix, classLabels, numIter=150):
-    m, n = dataMatrix.shape
+def stocGradAscent1(dataMat, labelMat, numIter=150):
+    m, n = dataMat.shape
     weights = np.ones(n)
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
             alpha = 4 / (1.0 + j + i) + 0.01
             randIndex = int(np.random.uniform(0, len(dataIndex)))
-            h = sigmoid(np.sum(dataMatrix[randIndex] * weights))
-            error = classLabels[randIndex] - h
-            weights = weights + alpha * error * dataMatrix[randIndex]
+            h = sigmoid(np.sum(dataMat[randIndex].dot(weights)))
+            error = labelMat[randIndex] - h
+            weights = weights + alpha * error * dataMat[randIndex]
             del(dataIndex[randIndex])
     return weights
 
-wei1 = stocGradAscent1(np.array(dataMatIn), classLabels)
+wei1 = stocGradAscent1(dataMat, labelMat)
