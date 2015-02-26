d400772d7fbc2fa806c8feebe2c60796eecf7fbf
Ji ZHANG <zhangji87@gmail.com>
2015-02-26 15:28:04 +0800

    performance

--- r/lr.r
+++ r/lr.r
@@ -7,13 +7,18 @@ points(data$x1[which(data$y == 1)], data$x2[which(data$y == 1)], pch=19)
 sol.glm <- glm(y ~ x1 + x2, data, family=binomial("logit"))
 print(summary(sol.glm))
 
-intercept <- coef(sol.glm)[1] / (-coef(sol.glm)[3])
-slope <- coef(sol.glm)[2] / (-coef(sol.glm)[3])
+intercept <- -coef(sol.glm)[1] / coef(sol.glm)[3]
+slope <- -coef(sol.glm)[2] / coef(sol.glm)[3]
 abline(intercept, slope)
 
+y <- ifelse(sol.glm$fitted.values >= 0.5, 1, 0)
+y <- as.factor(y)
+performance = length(which(y == data$y)) / nrow(data)
+print(paste("Performance: ", performance, sep=""))
+
 test.data <- data.frame(x1=c(45), x2=c(85))
-npv <- predict(sol.glm, test.data)
-npv <- 1 / (1 + exp(-npv))
-npv <- as.factor(ifelse(npv >= 0.5, 1, 0))
-test.data$y <- npv
+test.y <- predict(sol.glm, test.data)
+test.y <- 1 / (1 + exp(-test.y))
+test.y <- as.factor(ifelse(test.y >= 0.5, 1, 0))
+test.data$y <- test.y
 print(test.data)
